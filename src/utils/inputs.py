c97efc3a51ec61c316ac93a613397fa7eaae2e77
b-dienes <244570204+b-dienes@users.noreply.github.com>
2025-11-28 22:30:34 +0100

    Set test AOI and resolution

--- src/utils/inputs.py
+++ src/utils/inputs.py
@@ -12,9 +12,9 @@ def user_input():
     # USER INPUT:
     # SW and NE coordinates (lon, lat)
     # Resolution (min: 0.6 meter/pixel is the highest possible in NAIP)
-    sw_lat, sw_lon = 44.04, -121.33
-    ne_lat, ne_lon = 44.06, -121.30
-    resolution = 1.25
+    sw_lat, sw_lon = 44.05, -121.3285
+    ne_lat, ne_lon = 44.06, -121.3145
+    resolution = 0.6
 
     return UserInput(
         sw_lat = sw_lat,
