ad0df2ce8f4ca2f0b3e50479dd9f736c05a787d8
b-dienes <244570204+b-dienes@users.noreply.github.com>
2025-11-29 13:32:27 +0100

    Name files based on user input

--- src/utils/inputs.py
+++ src/utils/inputs.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 
 @dataclass
 class UserInput:
+    aoi_name: object
     sw_lat: object
     sw_lon: object
     ne_lat: object
@@ -12,11 +13,13 @@ def user_input():
     # USER INPUT:
     # SW and NE coordinates (lon, lat)
     # Resolution (min: 0.6 meter/pixel is the highest possible in NAIP)
+    aoi_name = "bend_oregon_test"
     sw_lat, sw_lon = 44.05, -121.3285
     ne_lat, ne_lon = 44.06, -121.3145
     resolution = 0.6
 
     return UserInput(
+        aoi_name = aoi_name,
         sw_lat = sw_lat,
         sw_lon = sw_lon,
         ne_lat = ne_lat,
